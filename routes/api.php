d41c30121fb028a372506448d7c32eac3141c425
KusumaDNC <59551120+KusumaDNC@users.noreply.github.com>
2020-01-20 15:54:24 +0700

    Update Rekap Pengaduan and Add API

--- routes/api.php
+++ routes/api.php
@@ -26,13 +26,8 @@ $api = app('Dingo\Api\Routing\Router');
 $api->version('v1',['namespace' => 'App\Http\Controllers'], function ($api){
     $api->get('login','API\APIAuthController@login');
     $api->post('login','API\APIAuthController@login');
-/*});
-$api->version('v1',['middleware'=>'jwt.auth', 'namespace' => 'App\Http\Controllers'], function ($api){*/
     $api->get('users', 'API\APIAuthController@index');
     $api->post('users', 'API\APIAuthController@index');
-    //$api->get('data=RekapOSS', 'API\RekapOssAPI@index');
-    //$api->get('/dpmptsp/invest', 'API\RekapInvestAPI@index');
-    //$api->get('/dpmptsp', 'API\RekapPerizinanAPI@index');
     $api->get('/dpmptsp', 'API\SDSControllerAPI@index');
     $api->post('logout','API\APIAuthController@logout');
 });
